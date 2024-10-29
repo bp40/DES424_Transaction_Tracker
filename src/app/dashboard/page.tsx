ae6612f698170d6a5ef25af41a3fd82b33731ed6
bp40 <60870525+bp40@users.noreply.github.com>
2024-10-30 00:02:50 +0700

    update navbar and dashboard page to check user is logged in

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,21 @@
-const Dashboard = () => {
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+
+const Dashboard = async () => {
+
+    const supabase = await createClient()
+
+    const { data, error } = await supabase.auth.getUser()
+    if (error || !data?.user) {
+        redirect('/auth/login')
+    }
+
     return (
-        <h1 className="p-2">
-            Hello, this is the Dashboard page
-        </h1>
+        <div className="py-20">
+            <h1 className="p-2 ">Hello, this is the Dashboard page</h1>
+            <h2>You are logged in as {data.user.email}</h2>
+        </div>
     )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,55 +1,129 @@
+"use client"
+
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import {useEffect, useState} from "react";
+import {createClient} from "@/utils/supabase/client";
 
 const Navbar = () => {
-    return (
-        <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
-            <div className="w-full max-w-7xl mx-auto px-4">
-                <div className="flex justify-between h-14 items-center">
-                    <Link href="#" className="flex items-center" prefetch={false}>
-                        <span className="sr-only">Acme Inc</span>
-                    </Link>
-                    <nav className="hidden md:flex gap-4">
-                        <Link
-                            href={{
-                                pathname: "/",
-                            }}
-                            className="font-medium flex items-center text-sm transition-colors hover:underline"
-                            prefetch={false}
-                        >
-                            Home
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const supabase = createClient()
+
+    useEffect(() => {
+
+        const fetchUser = async () => {
+            const {data: {user}} = await supabase.auth.getUser()
+
+            if (user) {
+                setIsLoggedIn(true)
+            } else {
+                setIsLoggedIn(false)
+            }
+
+        }
+        fetchUser()
+
+
+    }, [isLoggedIn])
+
+    if (!isLoggedIn) {
+        return (
+            <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
+                <div className="w-full max-w-7xl mx-auto px-4">
+                    <div className="flex justify-between h-14 items-center">
+                        <Link href="#" className="flex items-center" prefetch={false}>
+                            <span className="sr-only">Acme Inc</span>
                         </Link>
-                        <Link
-                            href={{
-                                pathname: "/dashboard",
-                            }}
-                            className="font-medium flex items-center text-sm transition-colors hover:underline"
-                            prefetch={false}
-                        >
-                            Dashboard
-                        </Link>
-                        <Link
-                            href={{
-                                pathname: "/documentation",
-                            }}
-                            className="font-medium flex items-center text-sm transition-colors hover:underline"
-                            prefetch={false}
-                        >
-                            Documentation
-                        </Link>
-                    </nav>
-                    <div className="flex items-center gap-4">
-                        <Button variant="outline" size="sm" >
-                            <Link href="/auth/login" className="flex items-center gap-2"> Sign In </Link>
-                        </Button>
-                        <Button size="sm" >
-                            <Link href="/auth/signup" className="flex items-center gap-2"> Sign Up </Link>
-                        </Button>
+                        <nav className="hidden md:flex gap-4">
+                            <Link
+                                href={{
+                                    pathname: "/",
+                                }}
+                                className="font-medium flex items-center text-sm transition-colors hover:underline"
+                                prefetch={false}
+                            >
+                                Home
+                            </Link>
+                            <Link
+                                href={{
+                                    pathname: "/dashboard",
+                                }}
+                                className="font-medium flex items-center text-sm transition-colors hover:underline"
+                                prefetch={false}
+                            >
+                                Dashboard
+                            </Link>
+                            <Link
+                                href={{
+                                    pathname: "/documentation",
+                                }}
+                                className="font-medium flex items-center text-sm transition-colors hover:underline"
+                                prefetch={false}
+                            >
+                                Documentation
+                            </Link>
+                        </nav>
+                        <div className="flex items-center gap-4">
+                            <Button variant="outline" size="sm">
+                                <Link href="/auth/login" className="flex items-center gap-2"> Sign In </Link>
+                            </Button>
+                            <Button size="sm">
+                                <Link href="/auth/signup" className="flex items-center gap-2"> Sign Up </Link>
+                            </Button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    } else {
+        return (
+            <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
+                <div className="w-full max-w-7xl mx-auto px-4">
+                    <div className="flex justify-between h-14 items-center">
+                        <Link href="#" className="flex items-center" prefetch={false}>
+                            <span className="sr-only">Acme Inc</span>
+                        </Link>
+                        <nav className="hidden md:flex gap-4">
+                            <Link
+                                href={{
+                                    pathname: "/",
+                                }}
+                                className="font-medium flex items-center text-sm transition-colors hover:underline"
+                                prefetch={false}
+                            >
+                                Home
+                            </Link>
+                            <Link
+                                href={{
+                                    pathname: "/dashboard",
+                                }}
+                                className="font-medium flex items-center text-sm transition-colors hover:underline"
+                                prefetch={false}
+                            >
+                                Dashboard
+                            </Link>
+                            <Link
+                                href={{
+                                    pathname: "/documentation",
+                                }}
+                                className="font-medium flex items-center text-sm transition-colors hover:underline"
+                                prefetch={false}
+                            >
+                                Documentation
+                            </Link>
+                        </nav>
+                        <div className="flex items-center gap-4">
+                            <Button variant="outline" size="sm">
+                                <Link href="/auth/logout" className="flex items-center gap-2"> Logout </Link>
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+
 }
 
 export default Navbar;
