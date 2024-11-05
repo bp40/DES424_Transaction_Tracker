d369689d40eee83730ac9c177c4d68c268c11987
bp40 <60870525+bp40@users.noreply.github.com>
2024-11-05 10:44:14 +0700

    replaced fixed ui element with dynamic mappings

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,6 @@ import {Button} from "@/components/ui/button";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import {IncomeChart} from "@/components/IncomeChart";
 import TransactionListBlock from "@/components/TransactionListBlock";
-import {Dialog, DialogTrigger} from "@/components/ui/dialog";
 import ManualAddTxModalButton from "@/app/dashboard/ManualAddTxModalButton";
 
 
@@ -18,6 +17,18 @@ const Dashboard = async () => {
         redirect('/auth/login')
     }
 
+    const recentTransactions = [
+        {amount: 299, category: "Food", payee:"Some restaurant", expense:true},
+        {amount: 15000, category: "Income", payee:"Job", expense:false},
+    ]
+
+    const cardInfo = [
+        {title: "Last Month Income", amount: 8000, percentage: 8},
+        {title: "Last Month Expense", amount: 6835, percentage: -12},
+        {title: "Recurring Expenses", amount: 349, percentage: 0},
+        {title: "Most Spent Category", amount: 0, percentage: 0},
+    ]
+
     return (
         <div className="py-20 h-screen justify-center">
             <div className="flex justify-between items-center mx-6">
@@ -29,38 +40,23 @@ const Dashboard = async () => {
             </div>
             <div className="flex flex-wrap h-screen sm:grid sm:grid-cols-5 sm:grid-rows-5 gap-4 content-center m-8">
                 <div className="grid grid-cols-subgrid gap-4 col-span-2 row-span-2 h-full">
-                    <DashboardCard
-                        className='col-start-1 col-end-2 row-start-1 row-end-2 h-full'
-                        title="Last Month Income"
-                        amount={8000}
-                        percentage={8}
-                    />
-                    <DashboardCard
-                        className='col-start-2 col-end-3 row-start-1 row-end-2'
-                        title="Last Month Expense"
-                        amount={6835}
-                        percentage={-12}
-                    />
-                    <DashboardCard
-                        className='col-start-1 col-end-2 row-start-2 row-end-3'
-                        title="Recurring Expenses"
-                        amount={349}
-                        percentage={0}
-                    />
-                    <DashboardCard
-                        className='col-start-2 col-end-3 row-start-2 row-end-3'
-                        title="Most Spent Category"
-                        amount={0}
-                        percentage={0}
-                    />
+                    { cardInfo.map((info) => (
+                        <DashboardCard
+                            amount={info.amount}
+                            percentage={info.percentage}
+                            title={info.title}
+                            key={info.title}
+                        />
+                        ))}
                 </div>
                 <Card className="col-start-1 col-end-3 row-start-3 row-end-6 m-2">
                     <CardHeader>
                         <CardTitle>Recent Transactions</CardTitle>
                     </CardHeader>
                     <CardContent>
-                        <TransactionListBlock amount={299} category="Food" payee="Some Restaurant" expense={true}/>
-                        <TransactionListBlock amount={15000} category="Income" payee="Job" expense={false}/>
+                        { recentTransactions.map((transaction) => (
+                            <TransactionListBlock key={transaction.amount + transaction.payee} amount={transaction.amount} payee={transaction.payee} category={transaction.category} expense={transaction.expense}/>
+                            ))}
                     </CardContent>
                 </Card>
                 <div className="col-start-3 col-end-6 row-start-1 row-end-4 m-2">
