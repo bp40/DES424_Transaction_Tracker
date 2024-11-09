6ef0fe8d6a773a7f21f62611afaf5bb84087046a
Ikkiu <phantomjik@gmail.com>
2024-11-09 17:02:58 +0700

    minor edit check

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 const Dashboard = () => {
     return (
         <h1 className="p-2">
-            Hello, this is the Dashboard page
+            Hello, this is the Dashboard page HHHHH
         </h1>
     )
 }
