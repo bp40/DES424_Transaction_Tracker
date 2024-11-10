7f47ea836fbba1752f23749e9f95f4effe7b12a8
bp40 <60870525+bp40@users.noreply.github.com>
2024-11-10 23:10:05 +0700

    fix alignment of recent transactions

--- src/components/TransactionListBlock.tsx
+++ src/components/TransactionListBlock.tsx
@@ -10,16 +10,22 @@ const TransactionListBlock = ({amount, payee, category, expense}: {
 
 
     return (
-        <Card className="w-full h-full p-4 flex flex-row justify-between my-2">
+        <Card className="w-full h-full p-4 grid grid-cols-3 items-center my-2">
             <h2 className={expense ? "text-red-500 text-xl" : "text-green-500 text-xl"}>
                 ฿ {amount.toLocaleString()}
             </h2>
-            <h2 className="text-xl text-gray-500"> {payee} </h2>
-            <Badge className={expense ? "bg-red-500" : "bg-green-500"}>
-                {category}
-            </Badge>
 
+            <h2 className="text-xl text-gray-500 text-center col-span-1">
+                {payee}
+            </h2>
+
+            <div className="flex justify-end col-span-1">
+                <Badge className={expense ? "bg-red-500" : "bg-green-500"}>
+                    {category}
+                </Badge>
+            </div>
         </Card>
+
     )
 }
 
